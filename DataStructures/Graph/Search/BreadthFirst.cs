6516bbcf544c07e3aa3bc0e7c5463e952a19cb99
Sena Nur Eren <senanur-eren-102@hotmail.com>
2023-05-30 13:40:44 +0300

    DisJointSet and Graph added.

---
+++ DataStructures/Graph/Search/BreadthFirst.cs
@@ -0,0 +1,47 @@
+﻿using Queue;
+using Queue.Contract;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph.Search
+{
+    public class BreadthFirst<T>
+    {
+        public bool Find(IGraph<T> graph, T vertexKey)
+        {
+            return bfs(graph.ReferenceVertex,
+                new HashSet<T>(),
+                vertexKey);
+        }
+
+        private bool bfs(IGraphVertex<T> referenceVertex,
+            HashSet<T> visited,
+            T searchVertexKey)
+        {
+            var Q = new LinkedListQueue<IGraphVertex<T>>();
+            Q.EnQueue(referenceVertex);
+            visited.Add(referenceVertex.Key);
+            while (Q.Count > 0)
+            {
+                var current = Q.DeQueue();
+                Console.WriteLine(current.Key);
+                if (current.Key.Equals(searchVertexKey))
+                    return true;
+
+                foreach (var edge in current.Edges)
+                {
+                    if (visited.Contains(edge.TargetVertexKey))
+                        continue;
+                    visited.Add(edge.TargetVertexKey);
+                    Q.EnQueue(edge.TargetVertex);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DataStructures/Graph/Search/DepthFirst.cs
@@ -0,0 +1,35 @@
+﻿namespace Graph.Search
+{
+    /// <summary>
+    /// Depth First Search
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DepthFirst<T>
+    {
+        public bool Find(IGraph<T> graph, T vertexKey)
+        {
+            return dfs(graph.ReferenceVertex,
+                new HashSet<T>(),
+                vertexKey);
+        }
+
+        private bool dfs(IGraphVertex<T> current,
+            HashSet<T> visited,
+            T searchVertexKey)
+        {
+            visited.Add(current.Key);
+            Console.WriteLine(current.Key);
+            if (current.Key.Equals(searchVertexKey))
+                return true;
+
+            foreach (var edge in current.Edges)
+            {
+                if (visited.Contains(edge.TargetVertexKey))
+                    continue;
+                if (dfs(edge.TargetVertex, visited, searchVertexKey))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
